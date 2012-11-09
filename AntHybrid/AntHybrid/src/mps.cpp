071749edb99130fa3b0b74cb4bec9448e50eb394
Hamilton Turner <hamiltont@gmail.com>
2012-11-09 03:31:28 -0500

    Starting to add code. Nothing is tested, but problem is roughly defined and some methods are implemented

--- AntHybrid/AntHybrid/src/mps.cpp
+++ AntHybrid/AntHybrid/src/mps.cpp
@@ -8,24 +8,40 @@
 
 #include "mps.h"
 
+#include <stdexcept>
+
 
 MpsProblem::MpsProblem(Matrix<unsigned int> *distances) {
+    cleanup();
     
+    // Sanity Checks
+    std::vector<Task>::iterator it;
+    unsigned int priority = 0;
+    for ( it=tasks_->begin() ; it < tasks_->end(); it++ ) {
+        if (priority > (*it).priority_)
+            throw "Task list is not sorted by priority";
+        
+        priority = (*it).priority_;
+    }
 }
 
 MpsProblem::~MpsProblem(){}
 
 unsigned int MpsProblem::get_max_tour_size(){
-    return 0;
+    return (int) (*tasks_).size();
 }
 
 unsigned int MpsProblem::number_of_vertices(){
-    return 0;
+    return (int) ((*tasks_).size() * (*cores_).size());
 }
 
 std::map<unsigned int,double> MpsProblem::get_feasible_start_vertices(){
-    std::map<unsigned int, double> foo;
-    return foo;
+
+    // Start on any processor, but you must execute the first task first
+    std::map<unsigned int,double> vertices;
+    unsigned int start = get_vertex_for(Util::random_number( (int) cores_->size()), 0);
+    vertices[start] = 1.0;
+    return vertices;
 }
 
 std::map<unsigned int,double> MpsProblem::get_feasible_neighbours(unsigned int vertex){
@@ -49,8 +65,36 @@ bool MpsProblem::is_tour_complete(const std::vector<unsigned int> &tour){
 }
 
 std::vector<unsigned int> MpsProblem::apply_local_search(const std::vector<unsigned int> &tour){
-    std::vector<unsigned int> foo;
-    return foo;
+    return tour;
 }
 
-void MpsProblem::cleanup(){}
+void MpsProblem::cleanup(){
+    next_ready_task_ = 0;
+
+    std::vector<Task>::iterator it;
+
+}
+
+unsigned int MpsProblem::get_vertex_for(unsigned int processor, unsigned int task) {
+    // unsigned: 0 to 4294967295
+    // Assumming that no one will run this with more than 9999 processors, so I'm
+    // reserving the 4 most significant bits for processor ID and all remaining
+    // LSBs for the taskID. If this goes production it should be changed
+    return (processor * 1000000) + task;
+}
+
+std::list<Task> MpsProblem::get_ready_tasks() {
+    int priority = (*tasks_)[next_ready_task_].priority_;
+    
+    std::list<Task> result;
+    for (int i = next_ready_task_ + 1; i < (*tasks_).size(); i++) {
+        if ((*tasks_)[i].priority_ == priority)
+            result.push_front((*tasks_)[i]);
+        else
+            break;
+    }
+    
+    return result;
+}
+
+
--- AntHybrid/AntHybrid/src/mps.h
+++ AntHybrid/AntHybrid/src/mps.h
@@ -13,13 +13,73 @@
 #include <cstdlib>
 #include <map>
 #include <string>
+#include "util.h"
 
 #include "ants.h"
 
 
+struct Task {
+    // 1 is 'highest' priority and will be executed before 2,
+    // which is before 3 ... etc
+    unsigned int priority_;
+        
+    unsigned int start_time_;
+    
+    unsigned int end_time_;
+    
+    // I find it convenient to store the array position of this
+    // Task inside of it
+    unsigned int id_;
+};
+
+struct Core {
+    
+    // Ordered from first to last
+    std::vector<Task*> tasks_;
+    
+    unsigned int id_;
+};
+
+
 /// Multiprocessor scheduling using the libaco implemenation.
 class MpsProblem : public OptimizationProblem {
-    public:
+private:
+
+    /// ============= Core Abstraction =============
+    
+    // Each task must be scheduled once, and tasks should be in
+    // ascending priority order before the aco begins execution
+    std::vector<Task>* tasks_;
+    
+    // libaco needs a fixed ordering
+    std::vector<Core>* cores_;
+    
+    /// ============= Removing Assumptions =========
+    
+    // TODO ignoring this for now...
+    // Execution times for running a task on a processor
+    //
+    // (*execution_times_)[v][w] gives you the time to run task v on processor w
+    // Matrix<unsigned int> * execution_times_;
+    
+    // TODO ignoring this for now...
+    // Routing times for passing messages between processors
+    //
+    // (*routing_times_)[v][w] gives you the time to pass a message from v to w
+    // Matrix<unsigned int> * routing_times_;
+    
+    /// ============= Implementation Variables =====
+    
+    // Index to the next task that should be executed. Note that when
+    // next_ready_task_ == tasks_.size() you have completed a tour
+    unsigned int next_ready_task_;
+    
+    unsigned int get_vertex_for(unsigned int processor, unsigned int task);
+    
+    // Returns all tasks with the same priority as next_ready_task_
+    std::list<Task> get_ready_tasks();
+
+public:
     MpsProblem(Matrix<unsigned int> *distances);
     ~MpsProblem();
     unsigned int get_max_tour_size();
