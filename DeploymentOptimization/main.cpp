5b575d6d0b02c3529a636806fb263a59153a72d1
Hamilton Turner <hamiltont@gmail.com>
2013-01-29 16:20:39 -0500

    Initial Commit

---
+++ DeploymentOptimization/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DeploymentOptimization
+//
+//  Created by Hamilton Turner on 1/29/13.
+//  Copyright (c) 2013 Virginia Tech. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
