180501552908ee4411d6e966d58830708450537e
Hamilton Turner <hamiltont@gmail.com>
2013-02-13 15:51:45 -0500

    Reads in STG, outputs generated MPI code

--- DeploymentOptimization/main.cpp
+++ DeploymentOptimization/main.cpp
@@ -10,17 +10,153 @@
 #include <boost/mpi/environment.hpp>
 #include <boost/mpi/communicator.hpp>
 #include <iostream>
+#include <fstream>
+#include "main.h" // Includes Task and STG parser
+
 namespace mpi = boost::mpi;
+
+// Forward declare methods to come
+void run_simple_mpi(int argc, char* argv[]);
+void build_mpi_from_stl(char* file_path, char* outfile_p);
+
+bool SAMPLE = false;
+
+// Simple usage: a.out <input_STG_file_path> <output_file_path>
+int main(int argc, char* argv[])
+{
+  if (SAMPLE)
+    run_simple_mpi(argc, argv);
+  else
+    build_mpi_from_stl(argv[1], argv[2]);
+  
+  return 0;
+}
+
+bool DEBUG_LOG = true;
+void build_mpi_case_for_task(unsigned int tid, unsigned int exectime, std::vector<unsigned int>& pre, std::vector<unsigned int>& post, std::ofstream& out);
+void build_mpi_from_stl(char* filepath, char* outfilepath) {
+  DirectedAcyclicGraph* task_precedence = NULL;
+  std::vector<Task>* tasks = parse_stg(filepath, task_precedence);
+  std::ofstream out;
+  out.open(outfilepath);
+  
+  // Write header
+  const char * header =
+    "#include <boost/mpi.hpp>\n"
+    "#include <boost/mpi/collectives.hpp>\n"
+    "#include <boost/mpi/environment.hpp>\n"
+    "#include <boost/mpi/communicator.hpp>\n"
+    "namespace mpi = boost::mpi;\n\n";
+  out << header;
+
+  const char * start_main =
+    "int main(int argc, char* argv[]) {\n"
+    "  mpi::environment env(argc, argv);\n"
+    "  mpi::communicator world;\n";
+  out << start_main;
+  
+  // Execution time array
+//  out <<
+//    "  long execution_times [" << tasks->size() << "] = {";
+//  
+//  unsigned long end = tasks->size();
+//  for (int i = 0; i < end; i++)
+//    out << tasks->at(i).execution_time_ <<
+//      (i==(end-1) ? "};\n\n" : ", ");
+  
+  // Switch start
+  out <<
+    "  switch (world.rank()) {\n";
+  
+  // build case (task_id, task_predecessors, task_sucessors, task_execution_time, out)
+  for (int i = 0; i < tasks->size(); i++) {
+    Task task = tasks->at(i);
+    std::vector<unsigned int> pre = task_precedence->get_predecessors(task.int_identifier_);
+    std::vector<unsigned int> post = task_precedence->get_successors(task.int_identifier_);
+    build_mpi_case_for_task(task.int_identifier_, task.execution_time_, pre, post, out);
+  }
+  
+  // Switch End
+  out <<
+    "  }\n"
+    "  return 0;\n"
+    "}\n\n";
+
+}
+
+void build_mpi_case_for_task(unsigned int tid, unsigned int exectime, std::vector<unsigned int>& pre, std::vector<unsigned int>& post, std::ofstream& out) {
+  
+  // ========= Case Header
+  out <<
+    "    case " << tid << ": {\n";
+  if (DEBUG_LOG)
+    out <<
+      "      std::cout << \"" << tid << ": Awake\" << std::endl;\n";
+
+  // ========= Receive Predecessors
+  if (pre.size() != 0) {
+    out <<
+      "      mpi::request req[" << pre.size() << "];\n";
+    for (int p = 0; p < pre.size(); p++)
+      out <<
+        "      req[" << p << "] = world.irecv(" << pre[p] << ", 0);\n";
+    out <<
+      "      mpi::wait_all(req, req + " << pre.size() << ");\n";
+
+    for (int p = 0; p < pre.size(); p++)
+      out <<
+        "      std::cout << \"" << tid << ": Recv notice from pred " << pre[p] << "\" << std::endl;\n";
+  }
+
+
+  // ========= Perform Computation
+  out <<
+    "      time_t start;\n"
+    "      start = time(NULL);\n";
+  
+  if (DEBUG_LOG)
+    out <<
+    "      std::cout << \"" << tid << ": Started compute\" << std::endl;\n";
+  out <<
+   "      while ((time(NULL)-start) < " << exectime << ");\n";
+  if (DEBUG_LOG)
+    out <<
+    "      std::cout << \"" << tid << ": Finished compute in \" << (time(NULL)-start) << std::endl;\n";
+  
+  
+  // ========= Send to Successors
+  if (post.size() != 0) {
+    out <<
+    "      mpi::request sreq[" << post.size() << "];\n";
+    for (int p = 0; p < post.size(); p++)
+      out <<
+      "      sreq[" << p << "] = world.isend(" << post[p] << ", 0);\n";
+    out <<
+    "      mpi::wait_all(sreq, sreq + " << post.size() << ");\n";
+    
+    for (int p = 0; p < post.size(); p++)
+      out <<
+      "      std::cout << \"" << tid << ": Sent notice to succ " << post[p] << "\" << std::endl;\n";
+  } else
+    out <<
+    "      std::cout << \"" << tid << ": DONE!!\" << std::endl;\n";
+
+  // ========= Case Footer
+  out <<
+    "      break; }\n";
+}
+
+
+
 
 // TODO: Sends need to be non-blocking. Perhaps also replace the current recv with receive all so that there are
 // not unexpected items. Also add in the ability to remove the data on the send
-int main(int argc, char* argv[])
-{
-
+void run_simple_mpi(int argc, char* argv[]) {
+  
   mpi::environment env(argc, argv);
   mpi::communicator world;
   long execution_times [4] = {1, 6, 1, 2};
-
+  
   switch (world.rank()) {
     case 0: {
       std::cout << "0: Awake" << std::endl;
@@ -28,6 +164,7 @@ int main(int argc, char* argv[])
       start = time (NULL);
       std::cout << "0: Started compute" << std::endl;
       while ((time(NULL)-start) < execution_times[0]);
+      
       std::cout << "0: Finished compute in " << (time(NULL)-start) << std::endl;
       
       mpi::request reqs[2];
@@ -78,6 +215,5 @@ int main(int argc, char* argv[])
       std::cout << "3: DONE!!" << std::endl;
       break; }
   }
-  
-  return 0;
 }
+
---
+++ DeploymentOptimization/main.h
@@ -0,0 +1,115 @@
+//
+//  main.h
+//  DeploymentOptimization
+//
+//  Created by Hamilton Turner on 2/13/13.
+//  Copyright (c) 2013 Virginia Tech. All rights reserved.
+//
+
+#ifndef DeploymentOptimization_main_h
+#define DeploymentOptimization_main_h
+
+#include "graph.h"
+
+struct Task {
+  // This is analogous to pred_level
+  // 1 is 'highest' priority and will be executed before 2,
+  // which is before 3 ... etc
+  unsigned int pred_level_;
+  
+  bool scheduled_;
+  
+  // TODO temporary, I need to implement the time matrix
+  unsigned int execution_time_;
+  
+  // A unique identifier for this Task, such as an ID from an
+  // external data file. If none is provided, this is initialized
+  // to a unique number
+  std::string identifier_;
+  
+  // Another possible unique identifier
+  unsigned int int_identifier_;
+  
+  Task() : pred_level_(0), scheduled_(false), execution_time_(0), identifier_("") {}
+  
+  const char* get_cstr() {
+    std::stringstream ss;
+    ss << "[" << identifier_ << "," << pred_level_ << "]";
+    return ss.str().c_str();
+  }
+};
+
+std::vector<Task>* parse_stg(const char *filepath, DirectedAcyclicGraph* &graph) {
+  enum section {
+    HEADER,
+    GRAPH,
+    FOOTER
+  };
+  
+  std::vector<Task>* tasks = NULL;
+  
+  section s = HEADER;
+  std::ifstream file(filepath);
+  
+  if(!file)
+    throw filepath;
+  
+  unsigned int task_count = 0;
+  while(file.good()) {
+    if(s == HEADER) {
+      file >> task_count;
+      if (task_count !=0) {
+        // STG format always includes two dummy nodes
+        task_count = task_count + 2;
+        graph = new DirectedAcyclicGraph(task_count);
+        tasks = new std::vector<Task>(task_count);
+        s = GRAPH;
+      }
+    } else if(s == GRAPH) {
+      if (task_count-- == 0) {
+        s = FOOTER;
+        continue;
+      }
+      unsigned int task_id, task_processing_time, predecessor_count;
+      file >> task_id;
+      file >> task_processing_time;
+      file >> predecessor_count;
+      
+      Task* task = &(tasks->at(task_id));
+      if (task_id == 0)
+        task->pred_level_ = 1;
+      
+      for (int i = 0; i < predecessor_count; i++) {
+        unsigned int pred_id;
+        file >> pred_id;
+        graph->add_edge(pred_id, task_id);
+        
+        task->pred_level_ = std::max(task->pred_level_,
+                                     tasks->at(pred_id).pred_level_ + 1);
+      }
+      
+      task->execution_time_ = task_processing_time;
+      std::ostringstream oss;
+      oss << "t" << task_id;
+      task->identifier_ = oss.str();
+      task->int_identifier_ = task_id;
+      
+    } else if (s == FOOTER) {
+      std::string line;
+      file >> line;
+    }
+  }
+  
+  file.close();
+  
+  // I want to keep my start and end tasks, so that I know the DAG begins at
+  // (*graph)[0][0] and I can request successors from there
+  //std::sort(tasks->begin(), tasks->end(), precedence_sort);
+  //tasks->pop_back();
+  //tasks->erase(tasks->begin());
+  
+  return tasks;
+}
+
+
+#endif
