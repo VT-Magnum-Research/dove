2492f95fd2c8e5b61ceb19858e4854db8feab2cd
Hamilton Turner <hamiltont@gmail.com>
2013-03-25 19:18:09 -0400

    Next round of changes for abdallah

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -7,6 +7,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
+// XML parsing
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+#include "rapidxml_print.hpp"
+
+// XML file used describe the deployments
+// TODO please set this up in parse_options
+rapidxml::xml_document<> xml;
+
+// Command-line argument parsing
 #include "libs/tclap/CmdLine.h"
 
 #define NUM_EVENTS 1
@@ -39,6 +50,33 @@ main(int argc,char* argv[])
   int ranks=get_rank_count();
   int fl_count=get_rankfile_count();
   cout << ranks << fl_count << endl;
+// TODO Instead of "looping on the rankfiles", make this read from 
+// the deployments.xml document. Look for 
+// all deployment tags, read the 'id' attribute from 
+// them, and assume that a rankfile exists that has 
+// the name rankfile.<id> that corresponds to that 
+// deployment. Use code like this: 
+//
+// Locate all deployments
+//  rapidxml::xml_node<>* deps = dep_doc.first_node("optimization")->
+//  first_node("deployments");
+//  for (rapidxml::xml_node<>* deployment = deps->first_node();
+//       deployment;
+//       deployment = deployment->next_sibling()) {
+//    
+//    // Build the rankfile path
+//    std::string id = deployment->first_attribute("id")->value();
+//    std::string rankfile = outdir;
+//    rankfile.append("rankfile.").append(id);
+//
+//    // TODO run kbest here, 
+//
+//    // TODO then add a new child node to the 
+//    // deployment called kbest. 
+//    // See http://rapidxml.sourceforge.net/manual.html#namespacerapidxml_1modifying_dom_tree
+//    // for some examples of how to add a  new node
+//  }
+
   //loop on the rank files, for each iteration, k-best scheme is applied
   int i;
   for(i=0;i<fl_count;i++)
@@ -52,10 +90,12 @@ main(int argc,char* argv[])
 static void parse_options(int argc, char *argv[]) 
 {                            
   TCLAP::CmdLine cmd("Multi-core Deployment Optimization Model -->  Runner",' ',"1.0");
-  TCLAP::ValueArg<std::string> inp_arg("i", "input_path", "path to rank files and other input files",false,"ranks/","rank files path");
-  cmd.add(inp_arg);
-  TCLAP::ValueArg<std::string> out_arg("o", "output_path", "path of output directory where ranlfiles.temp is written",false,"ranks/","output directory path");
-  cmd.add(out_arg);
+  TCLAP::ValueArg<std::string> inp_arg("i", "inputdir", "Path to directory containing rankfiles, deployment.xml. Must end with a slash", true, "ranks/", "inputdir path", cmd);
+  // TODO it's not clear what rankfiles.temp is or why we need an output directory path
+  // for it. If this is only needed internally for running MPI, then please just use the 
+  // standard C++ mkstemp command internally. There is example code for this at 
+  // latency/main_bootstrapper.cpp:93:    fd = mkstemp(sfn);
+  TCLAP::ValueArg<std::string> out_arg("o", "outputdir", "Path of output directory where rankfiles.temp is written", false, "ranks/", "output directory path", cmd);
   TCLAP::ValueArg<int> k_arg("k", "kvalue", "minimum number of runs",false,3,"integer: k value of k best scheme"); 
   cmd.add(k_arg);
   TCLAP::ValueArg<int> m_arg("m", "maximum", "maximum number of runs",false,10,"integer: M value of k best scheme"); 
@@ -68,14 +108,38 @@ static void parse_options(int argc, char *argv[])
   k=k_arg.getValue();
   M=m_arg.getValue();
   E=e_arg.getValue();
+
+  // TODO use rapidxml to parse the deployments.xml document, using 
+  // something like this: 
+  //
+  // Ensure that the given XML path is accessible by rapidxml
+  //try {
+  //  rapidxml::file<> xml_file(xml_arg.getValue().c_str());
+  //  xml.parse<0>(xml_file.data());
+  //} catch (rapidxml::parse_error err) {
+  //  std::cout << "Could not parse XML file. Error was: " << std::endl;
+  //  std::cout << err.what() << std::endl;
+  //  TCLAP::ArgException e("Unable to parse XML", err.what());
+  //  throw e;
+  //} 
 }
 
 //returns number of lines in a rankfile
 int get_rank_count()
 {
   ifstream fin;
+  // TODO - this should really be checked when parsing the 
+  // command line arguments. Can you please make rank_count
+  // and rankfile_count global variables and call these 
+  // functions inside of parse_options, throwing a TCLAP::ArgException
+  // if the necessary files are not available? 
   string filename = rank_path+"rankfile.0";
   fin.open(filename.c_str());
+  if (!fin) {
+    string error ("File was not readable: ");
+    error.append(filename);
+    throw error.c_str();
+  }
   string line;
   int ln_count=0;
 
