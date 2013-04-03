fa5cda2d6574d5bf03914dc3ef5a5940b1079ce9
Hamilton Turner <hamiltont@gmail.com>
2013-04-03 01:48:00 -0400

    Big update: links to dove; reads/writes xml files; prints progress; log functions
    - Makefile links to dove static library
    - Uses rapidxml to read system.xml and write updated xml including latency
    - Includes dryrun flag to avoid actually running MPI
    - Multiple logging functions and flags to enable
    - Calculates total work and reports progress

--- dove/profile_latency/main_bootstrapper.cpp
+++ dove/profile_latency/main_bootstrapper.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h> // mkstemp
+#include <iostream>
 
 // Provides regex and exec 
 #include "helper.hpp"
@@ -18,10 +19,18 @@
 #include "libs/rapidxml_utils.hpp"
 #include "libs/rapidxml_print.hpp"
 
-#include "dove.hpp"
+#include "dove.h"
 
-// Enable or disable debug statement printing
-#define DEBUG true
+// Each pass of the -v flag raises this by one
+// 0 prints errors
+// 1 prints info
+// 2 prints debug
+static int log_level = 0;
+void log(const char* msg, int level) {  if (level <= log_level) std::cout << 
+  "profile: " << msg << std::endl; }
+void info(const char* msg) { log(msg, 1); }
+void error(const char* msg) { log(msg, 0); }
+void debug(const char* msg) { log(msg, 2); }
 
 // Function declarations
 void calculate_latency(std::vector<int> ids);
@@ -33,7 +42,15 @@ void calculate_latency(std::vector<int> ids);
 static void parse_options(int argc, char *argv[]);
 
 // XML file used describe the system we are testing
-rapidxml::xml_document<> xml;
+rapidxml::xml_document<char>* xml;
+std::string xml_filepath;
+rapidxml::file<char>* xml_file;
+
+// If true, the latency is not actually profiled 
+static bool dry_run = false;
+
+// If true, progress will be printed as latency is calculated
+static bool print_progress = false;
 
 // Logical IDs of all hardware that should have
 // latency profiled. Each vector is used to generate
@@ -55,11 +72,36 @@ static std::vector<int> threads;
 //static std::vector<int> thread_filter;
 
 int main(int argc, char** argv) {
+  xml = new rapidxml::xml_document<char>();
   try {
     parse_options(argc, argv);
   } catch (TCLAP::ArgException &e) {
     std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
+    delete xml;
     exit(EXIT_SUCCESS);
+  }
+
+  
+  // TODO for the low-level filters, start by comparing the 
+  // lowest level items and work up. E.g. 
+  // for (threadpair in threads)
+  //     resolve all pieces of thread
+  //     for (filter in filters)
+  //        returnif (filter not passed)
+  //
+ 
+  // Prepare for writing a file
+  // TODO Create a temporary file at the start, flush updated XML to 
+  // the temporary file every latency measurement so we don't loose 
+  // everything if we crash
+  xml_filepath.append(".updated");
+  std::ofstream output(xml_filepath.c_str(), std::ios::trunc | std::ios::out);
+  if (!dry_run && !output.is_open())
+  {
+    error("Refusing to profile latency: Unable to open xml file for output:");
+    error(xml_filepath.c_str());
+    delete xml;
+    exit(EXIT_FAILURE);
   }
 
   // TODO update rapidxml_myutils to enable building of 
@@ -70,17 +112,13 @@ int main(int argc, char** argv) {
   //calculate_latency(threads);
   calculate_latency(cores);
 
-  // TODO for the low-level filters, start by comparing the 
-  // lowest level items and work up. E.g. 
-  // for (threadpair in threads)
-  //     resolve all pieces of thread
-  //     for (filter in filters)
-  //        returnif (filter not passed)
-  //
-  
-  // TODO build a model of progress by calculating the total number of 
-  // permutations for each hardwrae component type e.g. (# cores P 2)
-  
+  if (!dry_run) {
+    output << *xml;
+    output.close();
+  }
+
+
+  delete xml;
   exit(EXIT_SUCCESS);
 }
 
@@ -108,8 +146,16 @@ std::string make_rankfile(std::string to, std::string from) {
     return std::string(sfn);
 }
 
+// Makes a string 'safe' for use with rapidxml
+char* s(const char* unsafe) {
+  return xml->allocate_string(unsafe);
+}
+
+// Given the two and from, this writes them into the XML file at
+// the proper location and also returns the XML string written
+//
 // TODO accept int, int
-std::string get_latency(std::string from, std::string to) {
+std::string write_latency(std::string from, std::string to) {
     using namespace std;
 
     string mpirun_bin = "mpirun";
@@ -122,10 +168,8 @@ std::string get_latency(std::string from, std::string to) {
 #   endif
     string command = mpirun_bin + " " + mpiflags + " " + latency_bin;
     string result("-1");
-// TODO make this a command-line option
-#define DRY_RUN false
-    if (DRY_RUN)
-      cout << command << endl;
+    if (dry_run)
+      info(command.c_str());
     else
       result = exec(command);
     
@@ -137,12 +181,56 @@ std::string get_latency(std::string from, std::string to) {
     //string microsec_line = match(result, regex_line);
     //string microsec_str = match(microsec_line, regex_number);
     remove(rankfile.c_str());
-    return "<cd f=\"" + from +
-           "\"  t=\""   + to +
-           "\"  v=\""   + result + "\" />";
+    if (!dry_run) {
+      rapidxml::xml_node<char>* delay = xml->
+        allocate_node(rapidxml::node_element, "d");
+      rapidxml::xml_attribute<> *fattr = xml->allocate_attribute("f", 
+          s(from.c_str()));
+      rapidxml::xml_attribute<> *tattr = xml->allocate_attribute("t", 
+          s(to.c_str()));
+      rapidxml::xml_attribute<> *vattr = xml->allocate_attribute("v", 
+          s(result.c_str()));
+      delay->append_attribute(fattr);
+      delay->append_attribute(tattr);
+      delay->append_attribute(vattr);
+
+      // Find the right place in the XML
+      rapidxml::xml_node<char>* system = xml->first_node("system");
+      if (system == 0)
+      {
+        error("Unable to find system!");
+        error("Printing all XML so we can debug....");
+        rapidxml::print(std::cerr, *xml, 0);
+        error("Now to segfault...");
+      }
+      rapidxml::xml_node<char>* delays = system->first_node("routing_delays");
+      if (delays == 0) {
+        delays = xml->allocate_node(rapidxml::node_element, "routing_delays");
+        xml->first_node("system")->append_node(delays);
+      }
+    
+      delays->append_node(delay);
+
+      // Print to string using output iterator
+      std::string s;
+      rapidxml::print(std::back_inserter(s), *delay, 0);
+      return s;
+    }
+
+    // TODO update this to return the string without a value
+    return "<d f=\"" + from +
+      "\"  t=\""   + to +
+      "\"  v=\""   + result + "\" />";
 }
 
 void calculate_latency(std::vector<int> ids) {
+  // Use (N Permutation 2) formula, where N = ids.size
+  // However this makes huge numbers, so we take a shortcut and
+  // realize that N P 2 = N * N-1
+  unsigned long total = (unsigned long) ids.size()* ((unsigned long) ids.size() - 1);
+  unsigned long progress = 0;
+  time_t start = time(0);
+
   // TODO make previous functions accept integers 
   // First step is to calculate all permutations
   for (unsigned int i = 0; i < ids.size(); i++) {
@@ -154,10 +242,24 @@ void calculate_latency(std::vector<int> ids) {
             sj << ids[j];
             std::string ssi(si.str());
             std::string ssj(sj.str());
-            std::cout << get_latency(ssi,ssj) << std::endl;
+            info(write_latency(ssi,ssj).c_str());
+            progress++;
+            if (print_progress)
+            {
+              double p = (double) progress / (double) total;
+              p = p * 1000;
+              p = (double) ((int) p);
+              p = p / 10;
+              time_t end = time(0);
+              std::cout << "profile: Progress: " << p << "% (" << 
+                progress << "/" << total << ") in " <<
+                (int) difftime(end, start) <<
+                " seconds" << std::endl;
+            }
           }
       }
   }
+  info("Done calculating all latency!");
 }
 
 // Given parsed command-line options, this builds the lists that 
@@ -168,42 +270,38 @@ void build_main_filter(bool all, bool host, bool socket, bool core,
   typedef std::vector<xml_node*> xml_node_vector; 
 
   if (all || host) {
-    xml_node_vector xhosts = dove::get_all_hosts(xml);
+    xml_node_vector xhosts = dove::get_all_hosts(*xml);
     for (xml_node_vector::iterator it = xhosts.begin();
         it != xhosts.end();
         ++it)
     hosts.push_back(atoi((*it)->first_attribute("id")->value()));
   }
   if (all || socket) {
-    xml_node_vector xprocs = dove::get_all_processors(xml);
+    xml_node_vector xprocs = dove::get_all_processors(*xml);
     for (xml_node_vector::iterator it = xprocs.begin();
         it != xprocs.end();
         ++it)
     sockets.push_back(atoi((*it)->first_attribute("id")->value()));
   }
   if (all || core) {
-    xml_node_vector xcores = dove::get_all_cores(xml);
+    xml_node_vector xcores = dove::get_all_cores(*xml);
     for (xml_node_vector::iterator it = xcores.begin();
         it != xcores.end();
         ++it)
     cores.push_back(atoi((*it)->first_attribute("id")->value()));
   }
   if (all || thread) {
-    xml_node_vector xhw = dove::get_all_threads(xml);
+    xml_node_vector xhw = dove::get_all_threads(*xml);
     for (xml_node_vector::iterator it = xhw.begin();
         it != xhw.end();
         ++it)
     threads.push_back(atoi((*it)->first_attribute("id")->value()));
   }
 
-  // TODO using tclaps multi-switch, add the -d, -dd, -ddd flags for
-  // varying the level of debug output
-  if (DEBUG) {
-    std::cerr << "Size of host: " << hosts.size() << std::endl;
-    std::cerr << "Size of socket: " << sockets.size() << std::endl;
-    std::cerr << "Size of core: " << cores.size() << std::endl;
-    std::cerr << "Size of thread: " << threads.size() << std::endl;
-  }
+  //std::cerr << "Size of host: " << hosts.size() << std::endl;
+  //std::cerr << "Size of socket: " << sockets.size() << std::endl;
+  //std::cerr << "Size of core: " << cores.size() << std::endl;
+  //std::cerr << "Size of thread: " << threads.size() << std::endl;
 }
 
 // TODO add flag for output
@@ -216,6 +314,14 @@ static void parse_options(int argc, char *argv[]) {
       "logical IDs that are referenced from all other flags. Used to gather "
       "additional information about the system that is required for OpenMPI.",
       true, "system.xml", "filepath", cmd);
+  TCLAP::SwitchArg clear_xml("", "clearxml", "Removes all delay (d) tags from "
+      "the XML file. Effectively indicates that this profiling pass will be "
+      "overwriting any prior profiling information. Without this flag delay "
+      "tags will be appended. If two or more delay tags exist with the same "
+      "to/from values that will *not* throw errors, and later behaviour of "
+      "dove is undefined", cmd);
+  TCLAP::SwitchArg show_progress("p", "progress", "With this flag, progress "
+      "will be reported as the program is executing", cmd);
 
   // Setup the list of required filters
   TCLAP::SwitchArg all_filter("", "all", "Indicates that latency tests will "
@@ -247,6 +353,15 @@ static void parse_options(int argc, char *argv[]) {
   filter_list.push_back(&core_filter);
   filter_list.push_back(&thread_filter);
   cmd.xorAdd(filter_list);
+
+  TCLAP::SwitchArg dry_filter("", "dryrun", "No latency tests will "
+      "be performed, but all mpirun commands will be printed and temporary "
+      "files will be created. Implies at least one instance of the verbosity "
+      "flag e.g. -v. Passed XML file will not be modified at all", cmd);
+  TCLAP::MultiSwitchArg verbosity("v","verbose","Enables printing of any "
+    "verbose ouptut. Passing the flag multiple times e.g. -vvv increases "
+    "verbosity further. With no -v flag only errors or final summaries "
+    "are printed", cmd, 0);
 
   // List all of the partial filters that are possible
   /*TCLAP::MultiArg<int> node_arg("n", "node", "An additional filter for "
@@ -282,14 +397,37 @@ static void parse_options(int argc, char *argv[]) {
 
   // Ensure that the given XML path is accessible by rapidxml
   try {
-    rapidxml::file<> xml_file(xml_arg.getValue().c_str());
-    xml.parse<0>(xml_file.data());
+    info("Trying to parse the following xml file:");
+    xml_filepath = xml_arg.getValue();
+    info(xml_filepath.c_str());
+    xml_file = new rapidxml::file<char>(xml_arg.getValue().c_str());
+    xml->parse<0>(xml_file->data());
   } catch (rapidxml::parse_error err) {
     std::cout << "Could not parse XML file. Error was: " << std::endl;
     std::cout << err.what() << std::endl;
     TCLAP::ArgException e("Unable to parse XML", err.what());
     throw e;
-  } 
+  }
+
+  print_progress = show_progress.getValue();
+  dry_run = dry_filter.getValue();
+  log_level = verbosity.getValue();
+  // Require at least info logging if we are doing a dry run
+  if (dry_run && log_level==0)
+    log_level = 1;
+
+  if (clear_xml.getValue()) {
+    if (dry_run) 
+      info("Refusing to clear XML due to this being a dry run");
+    else {
+      rapidxml::xml_node<char>* delays = 
+        xml->first_node("system")->first_node("routing_delays");
+      if (delays != 0) {
+        xml->first_node("system")->remove_node(delays);
+        info("Removed the routing_delays node from the xml");
+      }
+    }
+  }
 
   // Use the high-level filter (all, cores, etc) to read in the XML 
   // file and build a list of all items that need to be compared. 
